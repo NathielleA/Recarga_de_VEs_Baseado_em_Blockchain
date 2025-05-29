3bd65f851e64afa95652bebe727971a6ec1bd8cc
GHenryssg <ghgomes@uefs.com.br>
2025-05-29 16:14:11 -0300

    Testando blockchain

---
+++ backend/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+    "context"
+    "fmt"
+    "log"
+    "github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+    client, err := ethclient.Dial("http://localhost:8545")
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    blockNumber, err := client.BlockNumber(context.Background())
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Printf("Bloco atual: %d\n", blockNumber)
+}
